5feb108cbfaf9705b41430d37dbea628b2de9b84
DikkeTimo <62908209+DikkeTimo@users.noreply.github.com>
2022-09-09 09:28:34 +0200

    New card
    Nieuwe visitekaartje gemaakt!

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,12 @@ expandButton.addEventListener("click", expand);
 function expand() {
   document.body.classList.toggle("expand");
 }
+
+function myFunction() {
+  var x = document.getElementById("card2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
